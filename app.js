50e8a8d2866eeb45d4c0b02880f96a5ef396d2e7
mfedericoq <martinfquiroga@gmail.com>
2023-03-12 23:45:26 -0300

    Modificacion 1

--- app.js
+++ app.js
@@ -1,5 +1,25 @@
-//SALUDO PRESENTACION
-let person = prompt("Hola, ¿cuál es su nombre?");
-document.getElementById("myName");
+//saludo presentación
+const person = prompt("Hola, ¿cuál es su nombre?");
+let myName = document.getElementById("myName");
 myName.innerHTML = person;
+
+
+//botón manipulado
+
+class Button {
+    constructor(parentID, text) {
+          this.parentID = parentID;
+          this.text = text;
+    }
+    render() {
+        let myName = document.getElementById(this.parentID);
+        myName.innerHTML = "<button></button>";
+        let myTexto = this.text;
+
+    }
+}
+
+let myButton = new Button();
+myButton.render(myName, 12);
+
 

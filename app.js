21b55b6b501e66cdbd0f524ad68facd2c70def4b
mfedericoq <martinfquiroga@gmail.com>
2023-03-13 22:41:38 -0300

    Cambios en app.js

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
-//saludo presentación
+//Pedido de datos para botones
 const person = prompt("Hola, ¿cuál es su nombre?");
-let myName = document.getElementById("myName");
-myName.innerHTML = person;
+const edad = prompt("¿Cuál es tu edad?")
 
 
-//botón manipulado
+//Creación de botón, que llevará el nombre capturado por el prompt
 
 class Button {
     constructor(parentID, text) {
@@ -13,13 +12,34 @@ class Button {
     }
     render() {
         let myName = document.getElementById(this.parentID);
-        myName.innerHTML = "<button></button>";
-        let myTexto = this.text;
+        const myButtonHTML = myName.innerHTML + "<button>" + this.text + "</button>";
+        myName.innerHTML = myButtonHTML;
 
     }
 }
 
-let myButton = new Button();
-myButton.render(myName, 12);
+let myButton = new Button("myName", person);
+myButton.render();
+let myButton2 = new Button("myName", edad);
+myButton2.render();
 
+/*Creación del input que llevará los nombres capturados por el prompt
+como placeholder*/
 
+class Input {
+    constructor(parentID, text) {
+          this.parentID = parentID;
+          this.text = text;
+    }
+    render() {
+        let myName = document.getElementById(this.parentID);
+        const InputHTML = myName.innerHTML + "<input type='text' placeholder=" + this.text + " readonly>" + "<br>";
+        myName.innerHTML = InputHTML;
+
+    }
+}
+
+let myInput = new Input("myInputs", person);
+myInput.render();
+let myInput2 = new Input("myInputs", edad);
+myInput2.render();
